e546fee57a8d2520d77986b20b94d85d551ecaab
Dan Griffin <unknown7979@gmail.com>
2016-10-25 13:20:02 -0500

    Implemented code to generate the blur matrix A (q3 part a).

--- HW6.py
+++ HW6.py
@@ -18,7 +18,26 @@ import math
 def main():
 	global xsignal
 	xsignal = np.loadtxt('./xsignal.csv', delimiter=',')
+	A = generateBlurMatrix(6, 3)
+	print(A)
 
+
+'''
+bi = (1/k)(xi + xi-1 + xi-2 + ... + xi-k+1) + wi is a blur equation.
+This equation makes a matrix A (nxn) for b = Ax + w.
+'''
+def generateBlurMatrix(n, k):
+	A = np.zeros((n,n))
+
+	for row in range(0, n):
+		kind = row
+		for writeIndex in range(kind, row - k, -1):
+			if writeIndex < 0:
+				break
+			else:
+				A[row, writeIndex] = 1
+
+	return (1/float(k))*A
 
 
 
