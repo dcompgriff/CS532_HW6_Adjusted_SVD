e7410d8ed19d4bf9dc4ab1dbb254f1c1742cc74c
Dan Griffin <unknown7979@gmail.com>
2016-10-25 12:56:17 -0500

    Initial commit.

---
+++ HW6.py
@@ -0,0 +1,42 @@
+import numpy as np
+import numpy.linalg as linalg
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import math
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+	global xsignal
+	xsignal = np.loadtxt('./xsignal.csv', delimiter=',')
+
+
+
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
